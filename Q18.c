d80cfaf27c9f01c2f441de57dcffabfde15701a3
Shree Dubey <shreedubey1807@gmail.com>
2026-09-25 17:31:01 +0530

    Day-9

---
+++ Q18.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main()
+{
+    float percentage;
+
+    printf("Enter percentage: ");
+    scanf("%f", &percentage);
+
+    if(percentage < 0 || percentage > 100)
+    {
+        printf("Invalid percentage.\n");
+    }
+    else if(percentage >= 90)
+    {
+        printf("Grade A\n");
+    }
+else if(percentage >= 80)
+    {
+        printf("Grade B\n");
+    }
+    else if(percentage >= 70)
+    {
+        printf("Grade C\n");
+    }
+    else if(percentage >= 60)
+    {
+        printf("Grade D\n");
+    }
+    else
+    {
+        printf("Grade F\n");
+    }
+
+    return 0;
+}
