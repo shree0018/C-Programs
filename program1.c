e818e8f15004cea236fb997d3fd11111961e138d
Shree Dubey <shreedubey1807@gmail.com>
2026-09-25 15:44:24 +0530

    Day 1

---
+++ program1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+int a = 10;
+float b = 5.5;
+char c ='A';
+double d = 15.12345;
+
+printf("integer = %d\n", a);
+printf("float = %.2f\n",b);
+printf("character = %c\n",c);
+printf("double = %lf\n",d);
+
+return 0;
+}
